83d95eef525f09a44cc2541bf3ac72578e36c19c
Khang Nguyen <80811235+Khangs331735@users.noreply.github.com>
2022-05-16 18:05:50 +0930

    Update product-category.php

--- Controller/product-category.php
+++ Controller/product-category.php
@@ -1,1 +1,113 @@
+<?php require_once('homepage.php'); ?>
 
+<?php
+if( !isset($_REQUEST['id']) || !isset($_REQUEST['type']) ) {
+    header('location: index.php');
+    exit;
+} else {
+
+    if( ($_REQUEST['type'] != 'top-category') && ($_REQUEST['type'] != 'mid-category') && ($_REQUEST['type'] != 'end-category') ) {
+        header('location: index.php');
+        exit;
+    } else {
+
+        $statement = $db->prepare("SELECT * FROM ABCtop_categories");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $top[] = $row['ABCtop_id'];
+            $top1[] = $row['ABCtop_name'];
+        }
+
+        $statement = $pdo->prepare("SELECT * FROM ABCmid_categories");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $mid[] = $row['ABCmid_id'];
+            $mid1[] = $row['ABCmid_name'];
+            $mid2[] = $row['ABCtop_id'];
+        }
+
+        $statement = $pdo->prepare("SELECT * FROM ABCend_categories");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+        foreach ($result as $row) {
+            $end[] = $row['ABCend_id'];
+            $end1[] = $row['ABCend_name'];
+            $end2[] = $row['ABCmid_id'];
+        }
+
+        if($_REQUEST['type'] == 'top-category') {
+            if(!in_array($_REQUEST['id'],$top)) {
+                header('location: index.php');
+                exit;
+            } else {
+
+                // Getting Title
+                for ($i=0; $i < count($top); $i++) { 
+                    if($top[$i] == $_REQUEST['id']) {
+                        $title = $top1[$i];
+                        break;
+                    }
+                }
+                $arr1 = array();
+                $arr2 = array();
+                // Find out all ecat ids under this
+                for ($i=0; $i < count($mid); $i++) { 
+                    if($mid2[$i] == $_REQUEST['id']) {
+                        $arr1[] = $mid[$i];
+                    }
+                }
+                for ($j=0; $j < count($arr1); $j++) {
+                    for ($i=0; $i < count($end); $i++) { 
+                        if($end2[$i] == $arr1[$j]) {
+                            $arr2[] = $end[$i];
+                        }
+                    }   
+                }
+                $final_ecat_ids = $arr2;
+            }   
+        }
+
+        if($_REQUEST['type'] == 'mid-category') {
+            if(!in_array($_REQUEST['id'],$mid)) {
+                header('location: index.php');
+                exit;
+            } else {
+                // Getting Title
+                for ($i=0; $i < count($mid); $i++) { 
+                    if($mid[$i] == $_REQUEST['id']) {
+                        $title = $mid1[$i];
+                        break;
+                    }
+                }
+                $arr2 = array();        
+                // Find out all ecat ids under this
+                for ($i=0; $i < count($end); $i++) { 
+                    if($end2[$i] == $_REQUEST['id']) {
+                        $arr2[] = $end[$i];
+                    }
+                }
+                $final_ecat_ids = $arr2;
+            }
+        }
+
+        if($_REQUEST['type'] == 'end-category') {
+            if(!in_array($_REQUEST['id'],$end)) {
+                header('location: index.php');
+                exit;
+            } else {
+                // Getting Title
+                for ($i=0; $i < count($end); $i++) { 
+                    if($end[$i] == $_REQUEST['id']) {
+                        $title = $end1[$i];
+                        break;
+                    }
+                }
+                $final_ecat_ids = array($_REQUEST['id']);
+            }
+        }
+        
+    }   
+}
+?>
